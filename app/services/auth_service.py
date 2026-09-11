0375aed65c5e5aca0d8c90cff1de636140dc8096
azareelamoki <azareelamok944@gmail.com>
2026-09-11 23:00:10 +0200

    projetc working tree rebased

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,16 +1,16 @@
-# from app.core.security import verify_password, hash_password, create_access_token
-# from app.repositories.user_repo import UserRepository
+from app.core.security import verify_password, hash_password, create_access_token
+from app.repositories.user_repo import UserRepository
 
-# class AuthService:
-#     def __init__(self, repo: UserRepository):
-#         self.repo = repo
+class AuthService:
+    def __init__(self, repo: UserRepository):
+        self.repo = repo
 
-#     def register(self, email: str, password: str):
-#         hashed = hash_password(password)
-#         return self.repo.create(email=email, hashed_password=hashed)
+    def register(self, email: str, password: str):
+        hashed = hash_password(password)
+        return self.repo.create(email=email, hashed_password=hashed)
 
-#     def authenticate(self, email: str, password: str):
-#         user = self.repo.get_by_email(email)
-#         if not user or not verify_password(password, user.hashed_password):
-#             return None
-#         return create_access_token({"sub": str(user.id)})
+    def authenticate(self, email: str, password: str):
+        user = self.repo.get_by_email(email)
+        if not user or not verify_password(password, user.hashed_password):
+            return None
+        return create_access_token({"sub": str(user.id)})
