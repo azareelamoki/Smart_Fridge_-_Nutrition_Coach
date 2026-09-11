6d7400c58822df0cd899e8432e9db21e108708ef
robin <robin.martin67@ynov.com>
2026-09-11 16:29:13 +0200

    add user.py schemas

---
+++ app/schemas/user.py
@@ -0,0 +1,24 @@
+from enum import Enum
+from pydantic import BaseModel, Field
+
+class GoalEnum(str, Enum):
+    LOSS = "Perte"
+    MAINTENANCE = "Maintien"
+    GAIN = "Prise"
+
+class Gender(str, Enum):
+    MALE = "Homme"
+    FEMALE = "Femme"
+
+class ActivityLevel(str, Enum):
+    LIGHT = "leger"
+    MODERATE = "modere"
+    ACTIVE = "actif"
+
+class User(BaseModel):
+    weight_kg: int = Field(gt=0, lt=300)
+    height_cm: int = Field(gt=0, lt=250)
+    age: int = Field(gt=0, lt=120)
+    gender: Gender
+    activity_level: ActivityLevel
+    goal: GoalEnum
