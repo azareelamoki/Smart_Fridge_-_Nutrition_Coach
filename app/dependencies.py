29b72732ff0deb51b9b507a6d77081c350525942
azareelamoki <azareelamok944@gmail.com>
2026-09-14 02:42:45 +0200

    dependencies functions added to manage authentication by setting up key elements

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,6 +1,10 @@
 import httpx
 from fastapi import Request
-
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from app.db.deps import get_db
+from app.repositories.user_repo import UserRepository
+from app.services.auth_service import AuthService
 
 def get_mealdb_client(request: Request) -> httpx.AsyncClient:
     return request.app.state.mealdb_client
@@ -8,3 +12,9 @@ def get_mealdb_client(request: Request) -> httpx.AsyncClient:
 
 def get_fooddata_client(request: Request) -> httpx.AsyncClient:
     return request.app.state.fooddata_client
+
+def get_user_repo(db: Session = Depends(get_db)):                       # UserRepo is a class to handle users storing in a data base, here with depends we instanciate the class session with Depends() which directly inject the parameter needed (here: db, using get_db)
+    return UserRepository(db)
+
+def get_auth_service(repo: UserRepository = Depends(get_user_repo)):    
+    return AuthService(repo)
