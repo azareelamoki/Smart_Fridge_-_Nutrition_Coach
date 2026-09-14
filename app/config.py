85395af3bd71948b4a3bf2a07c36f4b2a89b2b9d
robin <robin.martin67@ynov.com>
2026-09-14 14:05:24 +0200

    remove comment

--- app/config.py
+++ app/config.py
@@ -6,9 +6,9 @@ class Settings(BaseSettings):
     secret_key: str
     ALGORITHM: str = "HS256"
 
-    # mealdb_base_url: str          # Je les ai juste mis en commentaire elles sont correctes tes fonctions
-    # fooddata_base_url: str        #  Je les ai juste mis en commentaire elles sont correctes tes fonctions
-    # fooddata_api_key: str         #  Je les ai juste mis en commentaire elles sont correctes tes fonctions
+    mealdb_base_url: str
+    fooddata_base_url: str
+    fooddata_api_key: str
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
     
